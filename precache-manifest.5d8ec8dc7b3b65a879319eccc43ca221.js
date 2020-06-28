fdf2c8ba6ed1474c89f4aad3e105a6991d545bd1
David Gabrielian <davidgabrielofficial@gamil.com>
2020-06-28 16:22:30 +0400

    Updates

---
+++ precache-manifest.5d8ec8dc7b3b65a879319eccc43ca221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02c317e3c7d45e6c5c498317ba575f2",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "9e2531516d4a433e74ea",
+    "url": "/React-Todo/static/css/main.462113af.chunk.css"
+  },
+  {
+    "revision": "f7597634b6e819123e05",
+    "url": "/React-Todo/static/js/2.4f4ca7ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo/static/js/2.4f4ca7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2531516d4a433e74ea",
+    "url": "/React-Todo/static/js/main.95339729.chunk.js"
+  },
+  {
+    "revision": "e2e527bd15107c5574ce",
+    "url": "/React-Todo/static/js/runtime-main.3024a2df.js"
+  }
+]);
